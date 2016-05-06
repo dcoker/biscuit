e90419f30506ba656d2aad19942533431b1cd3d1
Doug Coker <dcoker@gmail.com>
2016-05-06 01:15:57 -0700

    enable cross-compilation and shell tests under Travis

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,22 +4,23 @@
 #
 set -e
 REPOSITORY=/go/src/github.com/dcoker/biscuit/
-AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id)"
-AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key)"
-AWS_SESSION_TOKEN="$(aws configure get aws_session_token)"
+if [ "${CONTINUOUS_INTEGRATION}" != "true" ]; then
+  echo __ Running with credentials from biscuit-testing profile.
+  AWS_ACCESS_KEY_ID="$(aws configure --profile biscuit-testing get aws_access_key_id)"
+  AWS_SECRET_ACCESS_KEY="$(aws configure --profile biscuit-testing get aws_secret_access_key)"
+fi
 AWS_REGION=us-west-1
-KEY1=37793df5-ad32-4d06-b19f-bfb95cee4a35
-ARN1=arn:aws:kms:us-west-1:105770556716:key/${KEY1}
+KEY1=8a97cd86-54c8-4964-b9b3-4d5d6ae98139
+ARN1=arn:aws:kms:us-west-1:922329555442:key/${KEY1}
 ARN1_REGION=us-west-1
-KEY2=c0045b15-9880-4b17-84da-a35760e8a16f
-ARN2=arn:aws:kms:us-west-2:105770556716:key/${KEY2}
+KEY2=0f809ad7-ecd3-41a3-9d21-923195530c8a
+ARN2=arn:aws:kms:us-west-2:922329555442:key/${KEY2}
 ARN2_REGION=us-west-2
 
 function invoke_one() {
   docker run -t \
     -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-    -e AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
     -e AWS_REGION="${AWS_REGION}" \
     -e KEY1="${KEY1}" \
     -e ARN1="${ARN1}" \
