fffd10e8ea994e2bf0a8b531411697f1253a113e
Doug Coker <dcoker@gmail.com>
2016-11-14 11:11:42 -0800

    Fix typo in test 019.

--- tests/019_multiple_regions_both_fail.sh
+++ tests/019_multiple_regions_both_fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-biscuit put -f store.yaml password god --key-id "${ARN1}","${ARN2}"
+biscuit put -f corrupt1.yaml password god --key-id "${ARN1}","${ARN2}"
 sed -i "s@${ARN1_REGION}@xxx@g" corrupt1.yaml
 sed -i "s@${ARN2_REGION}@xxx@g" corrupt1.yaml
 biscuit get -f corrupt1.yaml password
