52b28cdf25d84415d5f36b432d6cf86eab6f770b
Franky W <frankywahl@users.noreply.github.com>
2021-08-21 11:45:11 +0200

    GoReleaser as a release strategy
    Switches from using gox to using GoReleaser to release binaries

--- cross.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-for rls in build/biscuit/{linux,darwin}*; do \
-  tar czf build/biscuit-$(echo ${rls} | cut -f3 -d/).tgz -C ${rls} biscuit; \
-done
-for rls in build/biscuit/windows*; do \
-  mv ${rls}/biscuit.exe build/biscuit-$(echo ${rls} | cut -f3 -d/).exe
-done
-ls -l build/
